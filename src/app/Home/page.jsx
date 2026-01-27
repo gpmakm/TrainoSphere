e3a8b5d9869d08816843fde4e5ca85255527586a
gpmakm <165992407+gpmakm@users.noreply.github.com>
2026-01-27 13:57:21 +0530

    Change header from 'TrainoSphere' to 'ApdaMitra'

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className='container'>
       <div id='Welcome' style={{display:'flex',flexDirection:'column', width:'auto',height:'auto', padding:8,margin:8}}>
         <div><p>Welcome to</p></div>
-        <div><h1>TrainoSphere</h1></div>
+        <div><h1>ApdaMitra</h1></div>
         <div>the training platform to teach you managing disasters</div>
       </div>
     </div>
